743287d3352026c32b71a47509a2c5681aa25425
Philoem <philoem24@gmail.com>
2024-01-20 14:52:17 +0100

    style: improve responsivity header

--- front-end/src/components/Header/index.tsx
+++ front-end/src/components/Header/index.tsx
@@ -5,26 +5,24 @@ const Header = ({ addressWallet }: { addressWallet: string }) => {
 	const { voter, connectingWallet } = useConnectWallet()
 
 	return (
-		<div className='container mx-auto'>
-			<nav className='h-[80px] flex justify-between items-center border border-gray-400 px-3 rounded-full fixed top-2 w-5/6'>
-				<h1 className='font-sans text-4xl text-center font-bold pl-2 text-gray-900'>Simply Vote</h1>
-				{voter ? (
-					<Button
-						disabled
-						text={`${addressWallet?.substring(0, 7)}...${addressWallet?.substring(
-							addressWallet.length - 3
-						)} connecté`}
-						className='w-2/12 px-3 rounded-full text-sm bg-[#1B5CFE]'
-					/>
-				) : (
-					<Button
-						text='Connect your wallet'
-						className='w-2/12 px-3 rounded-full text-sm bg-[#1B5CFE]'
-						onClick={() => connectingWallet()}
-					/>
-				)}
-			</nav>
-		</div>
+		<nav className='h-[80px] flex justify-between items-center border border-gray-400 px-4 rounded-full fixed top-2 w-[96%] ml-[2%]'>
+			<h1 className='font-sans text-4xl text-center font-bold pl-2 text-gray-900'>Simply Vote</h1>
+			{voter ? (
+				<Button
+					disabled
+					text={`${addressWallet?.substring(0, 7)}...${addressWallet?.substring(
+						addressWallet.length - 3
+					)}, connecté`}
+					className='w-auto px-1 rounded-full text-sm lg:text-base bg-[#1B5CFE] h-[50px] tracking-wide pl-[10px] pr-[10px]'
+				/>
+			) : (
+				<Button
+					text='Connect your wallet'
+					className='w-auto px-1 rounded-full text-sm lg:text-base bg-[#1B5CFE] h-[50px] tracking-wide pl-[10px] pr-[10px]'
+					onClick={() => connectingWallet()}
+				/>
+			)}
+		</nav>
 	)
 }
 
