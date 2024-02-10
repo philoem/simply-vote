8b1778b09e9779be4f3b6491d3d1812d019cda0e
Philoem <philoem24@gmail.com>
2024-02-10 18:08:58 +0100

    feat: update homepage

--- front-end/src/components/Homepage/index.tsx
+++ front-end/src/components/Homepage/index.tsx
@@ -12,10 +12,16 @@ const Homepage = () => {
 					<div className='md:container md:mx-auto prose'>
 						<h1 className='font-sans text-6xl font-bold mb-4 text-gray-900'>Simply Vote</h1>
 						<p className='font-sans text-lg text-gray-800'>
-							Système de vote électronique connecté à la blockchain Ethereum.
+							Système de vote connecté à la blockchain Ethereum testnet Sepolia.
 						</p>
 						<p className='font-sans text-lg text-gray-800'>
 							Personnalisable, sécurisé et transparent.
+						</p>
+						<p className='font-sans text-lg text-gray-800'>
+							Créateur du vote, vous pourrez créer et modifier vos votes et voter pour les autres votes.
+						</p>
+						<p className='font-sans text-lg text-gray-800'>
+							Pas besoin de créer un vote pour voter, vous pouvez le faire à tout moment.
 						</p>
 						<p className='font-sans text-lg mb-4 text-gray-800'>A vous de jouer!</p>
 						<Button
