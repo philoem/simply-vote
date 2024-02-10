a194687a7998691aa69da2d62401de9f1e8caa7f
Philoem <philoem24@gmail.com>
2024-02-10 19:53:00 +0100

    test: add tests to voting contrat

--- test/Voting.ts
+++ test/Voting.ts
@@ -3,11 +3,6 @@ import { expect } from 'chai';
 
 describe("Voting", function () {
   let owner: any;
-  let otherAccount: any;
-
-  // beforeEach(async function () {
-  //   [owner, otherAccount] = await ethers.getSigners();
-  // });
 
   it("Should create a new voting proposal with the specified details", async () =>{
     const Voting = await ethers.getContractFactory("Voting");
@@ -52,45 +47,57 @@ describe("Voting", function () {
     [owner] = await ethers.getSigners();
     const voting = await Voting.deploy();
     await voting.createVote("Title", "Description", 1707419520000, 1707999999999, "link1", "link2");
-    console.log('voting :>> ', voting);
 
     await voting.vote(1, 1, voting.target);
-    // await expect(voting.proposals(1)).to.be(true);
   });
 
-  // it("Should check if a specific voter has cast a vote for a given proposal", async function () {
-  //   const Voting = await ethers.getContractFactory("Voting");
-  //   const [owner, voter] = await ethers.getSigners();
-  //   const voting = await Voting.deploy(owner.address);
-  //   await voting.createVote("Title", "Description", 10, 20, "link1", "link2");
-  //   await voting.vote(1, 1, voter.address);
+  it("Should check if a specific voter has cast a vote for a given proposal", async function () {
+    const Voting = await ethers.getContractFactory("Voting");
+    const [voter] = await ethers.getSigners();
+    const voting = await Voting.deploy();
+    await voting.createVote("Title", "Description", 1707419520000, 1707999999999, "link1", "link2");
+    await voting.vote(1, 1, voter.address);
 
-  //   const hasVoted = await voting.checkIfVoted(1, 1, voter.address);
-  //   expect(hasVoted).to.equal(true);
-  // });
+    const hasVoted = await voting.getVoterHasVoted(1, 1, voter.address);
+    expect(hasVoted.idVote).to.equal(1);
+    expect(hasVoted.id).to.equal(1);
+    expect(hasVoted.voter).to.equal(voter.address);
+  });
 
-  // it("Should log the winner of a specific vote", async function () {
-  //   const Voting = await ethers.getContractFactory("Voting");
-  //   const [owner, voter] = await ethers.getSigners();
-  //   const voting = await Voting.deploy(owner.address);
-  //   await voting.createVote("Title", "Description", 10, 20, "link1", "link2");
-  //   await voting.vote(1, 1, voter.address);
-  //   await ethers.provider.send("evm_increaseTime", [100000]);
-  //   await ethers.provider.send("evm_mine");
+  it("Should log the winner of a specific vote", async function () {
+    const Voting = await ethers.getContractFactory("Voting");
+    const [voter] = await ethers.getSigners();
+    const voting = await Voting.deploy();
+    await voting.createVote("Title", "Description", 1707577560000, 1707584760000, "link1", "link2");
+    await voting.vote(1, 1, voter.address);
+    await ethers.provider.send("evm_increaseTime", [100000]);
+    await ethers.provider.send("evm_mine");
 
-  //   await expect(voting.logWinnerIs(1)).to.emit(voting, "WinnerIs");
-  // });
+    // await expect(voting.logWinnerIs(1)).to.emit(voting, "WinnerIs");
+  });
 
-  // it("Should return the winning proposal for a specific vote", async function () {
-  //   const Voting = await ethers.getContractFactory("Voting");
-  //   const [owner, voter] = await ethers.getSigners();
-  //   const voting = await Voting.deploy(owner.address);
-  //   await voting.createVote("Title", "Description", 10, 20, "link1", "link2");
-  //   await voting.vote(1, 1, voter.address);
-  //   await ethers.provider.send("evm_increaseTime", [100000]);
-  //   await ethers.provider.send("evm_mine");
+  it("Should return the winning proposal for a specific vote", async function () {
+    const Voting = await ethers.getContractFactory("Voting");
+    const [voter] = await ethers.getSigners();
+    const voting = await Voting.deploy();
+    await voting.createVote("Title", "Description", 1707576420000, 1707580020000, "link1", "link2");
+    await voting.vote(1, 1, voter.address);
+    // await ethers.provider.send("evm_increaseTime", [100000]);
+    // await ethers.provider.send("evm_mine");
 
-  //   const winningProposal = await voting.winningProposal(1);
-  //   expect(winningProposal).to.equal(1);
-  // });
+    const winningProposal = await voting.winningProposal(1);
+    expect(winningProposal).to.equal(1);
+  });
+
+  it('should return winning proposal', async function () {
+    const Voting = await ethers.getContractFactory("Voting");
+    const voting = await Voting.deploy();
+    
+    await voting.createVote("Title", "Description", 1707576420000, 1707580020000, "link1", "link2");
+    const [voter] = await ethers.getSigners();
+    await voting.vote(1, 1, voter.address);
+    
+    const result = await voting.winningProposal(1);
+    expect(result).to.equal(1);
+  });
 });
