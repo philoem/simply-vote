5e956eba99a7a82fa174d71086e84e7ad3d4be68
Philoem <philoem24@gmail.com>
2024-02-14 10:30:35 +0100

    test: update test

--- test/Voting.ts
+++ test/Voting.ts
@@ -3,7 +3,9 @@ import { expect } from 'chai';
 
 describe("Voting", function () {
   let owner: any;
+  let voter: any;
 
+  // To test the createVote function
   it("Should create a new voting proposal with the specified details", async () =>{
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
@@ -21,6 +23,7 @@ describe("Voting", function () {
     await expect(voting.createVote(title, description, startsAt, 0, link1, link2)).to.be.revertedWithCustomError(voting, "InvalidStartEndTimesError");
   });
 
+  // To test the getDetailsVote function
   it("Should get the details of a specific vote", async () => {
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
@@ -31,6 +34,7 @@ describe("Voting", function () {
     expect(voteDetails.description).to.equal("Description");
   });
 
+  // To test the updateVote function
   it("Should update the details of a specific vote", async function () {
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
@@ -42,15 +46,45 @@ describe("Voting", function () {
     expect(updatedDetails.description).to.equal("New Description");
   });
 
-  it("Should register a vote for a specific proposal", async function () {
+  // To test the vote function
+  it("should vote to increment choiceOne when _id is 1 and an another vote with different address and same vote with the same address", async function () {
     const Voting = await ethers.getContractFactory("Voting");
-    [owner] = await ethers.getSigners();
+    [owner, voter] = await ethers.getSigners();
     const voting = await Voting.deploy();
     await voting.createVote("Title", "Description", 1707419520000, 1707999999999, "link1", "link2");
+    await voting.vote(1, 1, owner.address);
+    const choiceOne = (await voting.voteStructsArray(0)).choiceOne;
+    expect(choiceOne).to.equal(1);
 
-    await voting.vote(1, 1, voting.target);
+    // Second vote to same index of vote with an another address voter
+    await voting.vote(1, 1, voter.address);
+    const choiceOneBis = (await voting.voteStructsArray(0)).choiceOne;
+    expect(choiceOneBis).to.equal(2);
+    
+    // Second vote to same index of vote with the same address voter expected to reverted with custom error
+    expect(voting.vote(1, 1, voter.address)).to.be.revertedWithCustomError(voting, "AlreadyVotedError");
   });
 
+  // To test the vote function
+  it("should vote to increment choiceTwo when _id is 2 and an another vote with different address and same vote with the same address", async function () {
+    const Voting = await ethers.getContractFactory("Voting");
+    [owner, voter] = await ethers.getSigners();
+    const voting = await Voting.deploy();
+    await voting.createVote("Title", "Description", 1707419520000, 1707999999999, "link1", "link2");
+    await voting.vote(1, 2, owner.address);
+    const choiceTwo = (await voting.voteStructsArray(0)).choiceTwo;
+    expect(choiceTwo).to.equal(1);
+
+    // Second vote to same index of vote with an another address voter
+    await voting.vote(1, 2, voter.address);
+    const choiceTwoBis = (await voting.voteStructsArray(0)).choiceTwo;
+    expect(choiceTwoBis).to.equal(2);
+
+    // Second vote to same index of vote with the same address voter expected to reverted with custom error
+    expect(voting.vote(1, 2, voter.address)).to.be.revertedWithCustomError(voting, "AlreadyVotedError");
+  });
+
+  // To test the checkIfVoted function
   it("Should check if a specific voter has cast a vote for a given proposal", async function () {
     const Voting = await ethers.getContractFactory("Voting");
     const [voter] = await ethers.getSigners();
@@ -58,37 +92,10 @@ describe("Voting", function () {
     await voting.createVote("Title", "Description", 1707419520000, 1707999999999, "link1", "link2");
     await voting.vote(1, 1, voter.address);
 
-    const hasVoted = await voting.getVoterHasVoted(1, 1, voter.address);
-    expect(hasVoted.idVote).to.equal(1);
-    expect(hasVoted.id).to.equal(1);
-    expect(hasVoted.voter).to.equal(voter.address);
+    expect(await voting.checkIfVoted(1, voter.address)).to.equal(true);
   });
 
-  it("Should log the winner of a specific vote", async function () {
-    const Voting = await ethers.getContractFactory("Voting");
-    const [voter] = await ethers.getSigners();
-    const voting = await Voting.deploy();
-    await voting.createVote("Title", "Description", 1707577560000, 1707584760000, "link1", "link2");
-    await voting.vote(1, 1, voter.address);
-    await ethers.provider.send("evm_increaseTime", [100000]);
-    await ethers.provider.send("evm_mine");
-
-    // await expect(voting.logWinnerIs(1)).to.emit(voting, "WinnerIs");
-  });
-
-  it("Should return the winning proposal for a specific vote", async function () {
-    const Voting = await ethers.getContractFactory("Voting");
-    const [voter] = await ethers.getSigners();
-    const voting = await Voting.deploy();
-    await voting.createVote("Title", "Description", 1707576420000, 1707580020000, "link1", "link2");
-    await voting.vote(1, 1, voter.address);
-    // await ethers.provider.send("evm_increaseTime", [100000]);
-    // await ethers.provider.send("evm_mine");
-
-    const winningProposal = await voting.winningProposal(1);
-    expect(winningProposal).to.equal(1);
-  });
-
+  // To test the winningProposal function
   it('should return winning proposal', async function () {
     const Voting = await ethers.getContractFactory("Voting");
     const voting = await Voting.deploy();
@@ -98,6 +105,7 @@ describe("Voting", function () {
     await voting.vote(1, 1, voter.address);
     
     const result = await voting.winningProposal(1);
-    expect(result).to.equal(1);
+    expect(result.choiceOne).to.equal(1);
+    expect(result.choiceTwo).to.equal(0);
   });
 });
